3aaef3f6e1823d847880eee2f4d117b46888b5b6
shubham kumar <clboy768@gmail.com>
2025-03-24 11:55:31 +0530

    updated pagination part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
     console.log(ctx);
 
     const [Allproducts, setAllproducts] = useState([]);
+
     const getData=async()=>{
         let res = await fetch('https://www.dummyjson.com/products?limit=0');
             let data = await res.json();
@@ -14,21 +15,58 @@ const Home = () => {
             setAllproducts(data.products)
     }
 
+
+    const [currenPage, setcurrenPage] = useState(1);
+    const itemPerPage = 10;
+    const lastIndex = itemPerPage * currenPage;
+    const firstIndex = lastIndex - itemPerPage;
+    let noOfBtn = Math.ceil(Allproducts.length / itemPerPage);
+    console.log(noOfBtn) //20
+
+    let slicedArr = Allproducts.slice(firstIndex, lastIndex);
+    console.log(slicedArr)
+
+    // let btnArr = [];
+
+    // for(let i = 1; i<=noOfBtn; i++){
+    //     btnArr.push(i)
+    // }
+
+    // console.log(btnArr)
+
+
+
     useEffect(()=>{
         getData()
     },[])
   return (
-    <div className='p-6 bg-amber-950 grid gap-8 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 '>
+
+    <div>
+ <div className='p-6 bg-amber-950 grid gap-8 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 '>
     {
-        Allproducts.map((ele,i)=>{
+        slicedArr.map((ele,i)=>{
             return <div className='flex bg-white gap-2 rounded-md p-3 flex-col justify-between items-center'>
                 <img src={ele.thumbnail} alt="" />
                 <p>{ele.title}</p>
                 <button onClick={()=>ctx.AddtoCart(ele)} className='bg-green-500 px-3 py-2 rounded-md text-white'>Add to Cart</button>
             </div>
         })
-    }
+    }  
     </div>
+
+<div className='flex justify-center'>
+
+{/*  ['', '', '', '', '', '','', '', '',''] */}
+<button className='bg-black text-white rounded-md p-1  mx-1'>Prev</button>
+{
+    Array(noOfBtn).fill('').map((ele,i)=>{
+        return <button onClick={()=>setcurrenPage(i+1)} className='bg-blue-500 rounded-md p-1 w-10'>{i+1}</button>
+    })
+}
+<button className='bg-black text-white rounded-md p-1 mx-1 '>Next</button>
+</div>
+    </div>
+   
   )
 }
 
